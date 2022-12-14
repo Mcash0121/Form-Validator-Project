703e31e449a0b0905b4b72ea5f70e7161f7af35d
Mcash0121 <79586631+Mcash0121@users.noreply.github.com>
2022-12-14 16:22:00 -0600

    editing changes

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
+const form = document.getElementById("form")
+const username = document.getElementById("username")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const password2 = document.getElementById("password2")
 
 // show input error  message
 function showError(input, message) {
-  const formControl = input.parentElement;
-  formControl.className = "form-control error";
-  const small = formControl.querySelector("small");
-  small.innerText = message;
+  const formControl = input.parentElement
+  formControl.className = "form-control error"
+  const small = formControl.querySelector("small")
+  small.innerText = message
 }
 
 //Show success outline
 function showSuccess(input) {
-  const formControl = input.parentElement;
-  formControl.className = "form-control success";
+  const formControl = input.parentElement
+  formControl.className = "form-control success"
 }
 
 //check email is valid
 
 function checkEmail(input) {
-  const re = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
+  const re = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/
   if (re.test(input.value.trim())) {
-    showSuccess(input);
+    showSuccess(input)
   } else {
-    showError(input, "Email is not Valid");
+    showError(input, "Email is not Valid")
   }
 }
 
@@ -33,46 +33,46 @@ function checkEmail(input) {
 function checkRequired(inputArr) {
   inputArr.forEach(function (input) {
     if (input.value.trim() === "") {
-      console.log(input.id);
-      showError(input, `${getFieldName(input)} is required`);
+      console.log(input.id)
+      showError(input, `${getFieldName(input)} is required`)
     } else {
-      showSuccess(input);
+      showSuccess(input)
     }
-  });
+  })
 }
 
 //check input length
 function checkLength(input, min, max) {
   if (input.value.length < min) {
-    showError(input, ` ${getFieldName(input)} must be at least ${min}`);
+    showError(input, ` ${getFieldName(input)} must be at least ${min}`)
   } else if (input.value.length > max) {
     showError(
       input,
       `${getFieldName(input)} must be less than ${max} characters`
-    );
+    )
   } else {
-    showSuccess(input);
+    showSuccess(input)
   }
 }
 
 //Check passwords match
 function checkPasswordsMatch(input1, input2) {
   if (input1.value !== input2.value) {
-    showError(input2, "Passwords do not Match");
+    showError(input2, "Passwords do not Match")
   }
 }
 
 //Get fieldname
 function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1)
 }
 //Even Listeners
 form.addEventListener("submit", function (e) {
-  e.preventDefault();
+  e.preventDefault()
 
-  checkRequired([username, email, password, password2]);
-  checkLength(username, 3, 15);
-  checkLength(password, 6, 25);
-  checkEmail(email);
-  checkPasswordsMatch(password, password2);
-});
+  checkRequired([username, email, password, password2])
+  checkLength(username, 3, 15)
+  checkLength(password, 6, 25)
+  checkEmail(email)
+  checkPasswordsMatch(password, password2)
+})
